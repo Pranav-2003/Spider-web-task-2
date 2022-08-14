d912689e91381bf83bd1d6b1c806bf1c26f1a10e
unknown <nellaipranav@gmail.com>
2022-08-14 19:48:07 +0530

    modified database

--- santa/__init__.py
+++ santa/__init__.py
@@ -8,7 +8,7 @@ app  = Flask(__name__)
 app.config['SECRET_KEY'] = '5bea12aecb8640f427d035193b6f7670'
  
  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///santa.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gmsucprazblqpk:e69554b485ca09626dbfa172848b1a34cfa2b7b5ff8004f893e1089fccb2c57e@ec2-44-193-178-122.compute-1.amazonaws.com:5432/de9r775n46n148'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
